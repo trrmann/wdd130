88a8f671a0c29c55b895fdccfa8525682ba895f8
trrmann <tracy.mann1@retailbusinessservices.com>
2022-12-03 23:11:29 -0500

    deployed

--- luchonio/scripts/verticle_tabs_with_slide_show.js
+++ luchonio/scripts/verticle_tabs_with_slide_show.js
@@ -15,12 +15,15 @@ function showSlides(n) {
   var i, tablinks, slides, dots, imageNumberClass;
   tablinks = document.getElementsByClassName("nav_link");
   slides = document.getElementsByClassName("mySlides");
-  dots = document.getElementsByClassName("demo");
+  dots = document.getElementsByClassName("slide-show-demo");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   if ((n == 1) || (n > slides.length)) { imageNumberClass = "tab1" }
   if (n == 2) { imageNumberClass = "tab2" }
-  if ((n == 3) || (n < 1)) { imageNumberClass = "tab3" }
+  if (n == 3) { imageNumberClass = "tab3" }
+  if (n == 4) { imageNumberClass = "tab4" }
+  if (n == 5) { imageNumberClass = "tab5" }
+  if ((n == 6) || (n < 1)) { imageNumberClass = "tab6" }
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }  
